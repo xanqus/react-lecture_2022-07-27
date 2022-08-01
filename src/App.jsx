65f6f0ed92dcd5d3c585a0b6eab8f1f164973391
xanqus <sbk98031816@gmail.com>
2022-08-01 15:09:58 +0900

    useEffect 최초 1회 실행

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,11 @@
 import React, { useState } from "react";
 import Counter from "./components/Counter";
-import Converter from "./components/Converter";
-import MyBtn from "./components/MyBtn";
 
 const App = () => {
   const [counter, setCounter] = useState(0);
   return (
     <>
       <Counter counter={counter} setCounter={setCounter} />
-      <Converter counter={counter} />
-      <hr />
-      <MyBtn
-        text={"1번 버튼"}
-        fontSize={"10px"}
-        isChecked={true}
-        alertMsg={"1번버튼 클릭됨"}
-      />
-      <MyBtn
-        text={"2번 버튼"}
-        fontSize={"15px"}
-        isChecked={false}
-        alertMsg={"2번버튼 클릭됨"}
-      />
-      <MyBtn
-        text={"3번 버튼"}
-        fontSize={"20px"}
-        isChecked={false}
-        alertMsg={"3번버튼 클릭됨"}
-      />
     </>
   );
 };
--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,16 +1,22 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 const Counter = ({ counter, setCounter }) => {
   const addCounter = () => {
     setCounter(counter + 1);
   };
+  console.log(counter);
+  // API 호출 코드
+
+  useEffect(() => {
+    console.log("API 호출");
+  }, []);
+
   return (
     <div>
       <div>{counter}번 클릭했습니다</div>
       <button onClick={addCounter}>+</button>
     </div>
   );
-  return;
 };
 
 export default Counter;
