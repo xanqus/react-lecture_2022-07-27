cd5e27b98a995c02903fe725c4b46ff6ab98c41f
xanqus <sbk98031816@gmail.com>
2022-08-01 15:19:03 +0900

    useEffect deps

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,18 +1,36 @@
 import React, { useState, useEffect } from "react";
 
 const Counter = ({ counter, setCounter }) => {
+  const [searchWord, setSearchWord] = useState("");
   const addCounter = () => {
     setCounter(counter + 1);
   };
-  console.log(counter);
-  // API 호출 코드
+  const onChange = (e) => {
+    setSearchWord(e.target.value);
+  };
 
+  // 검색시 호출
   useEffect(() => {
+    // API 호출 코드
     console.log("API 호출");
   }, []);
 
+  useEffect(() => {
+    console.log("change input");
+  }, [searchWord]);
+
+  useEffect(() => {
+    console.log(counter);
+  }, [counter]);
+
   return (
     <div>
+      <input
+        type="text"
+        onChange={onChange}
+        value={searchWord}
+        placeholder="검색어"
+      />
       <div>{counter}번 클릭했습니다</div>
       <button onClick={addCounter}>+</button>
     </div>
